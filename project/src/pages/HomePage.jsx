e55599b2d5b0a40b2afda7a20183684352642e8d
Alfin Febrian <20k10020@student.unika.ac.id>
2023-05-06 11:31:49 +0700

    fix(HomePage): fix navigate route on button [See More] primary listing on desktop view

--- project/src/pages/HomePage.jsx
+++ project/src/pages/HomePage.jsx
@@ -160,7 +160,7 @@ function HomePage() {
           </div>
           <button
             type="button"
-            onClick={() => navigate('/dijual/prrimary')}
+            onClick={() => navigate('/dijual/primary')}
             className="text-blue-700 hidden md:flex mr-0 my-auto items-center gap-3 h-fit hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
           >
             See More <ArrowRight className="text-lg" />
